27f7d0e0fb4803c66068560fdb260f11bd0f88d3
oraziorillo <oraziorillo@gmail.com>
2025-06-25 19:47:21 +0200

    cleaned repo

--- wikipedia_test.py
+++
@@ -1,5 +0,0 @@
-import wikipedia
-from tools.wikipedia.utils import extract_context
-page = wikipedia.page("mercedes sosa")
-with open("clean_file.html", "w") as fp:
-    fp.write(extract_context(page))
