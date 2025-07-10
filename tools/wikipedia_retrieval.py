a78aabfcf7665acbb6a1bf1bc757b640a8413f37
oraziorillo <oraziorillo@gmail.com>
2025-07-10 19:04:00 +0200

    registered wikipedia tools with new tool registry

--- tools/wikipedia_retrieval.py
+++ tools/wikipedia_retrieval.py
@@ -3,15 +3,48 @@ import wikipedia
 from bs4 import BeautifulSoup
 from typing import Iterable
 from langchain_core.documents import Document
+from tools.tool_registry import register_tool
 from services.chroma import ChromaClient
 
 # Similarity retriever
 pages_cache: dict[str, wikipedia.WikipediaPage] = {}
 chroma_client = ChromaClient()
 
+
+@register_tool(
+    type = "function",
+    name = "wikipedia_page_search",
+    description = "Tool that searches for a Wikipedia page based on a query.",
+    parameters = {
+        "type": "object",
+        "properties": {
+            "query": {
+                "type": "string",
+                "description": "The query to search for a Wikipedia page."
+            }
+        },
+        "required": ["query"]
+    }
+)
 def wikipedia_page_search(query: str):
     return wikipedia.search(query)
+
     
+@register_tool(
+    type = "function",
+    name = "wikipedia_page_sections_retriever",
+    description = "Tool that retrieves the sections of a specific Wikipedia page.",
+    parameters = {
+        "type": "object",
+        "properties": {
+            "page_title": {
+                "type": "string",
+                "description": "The title of the Wikipedia page to retrieve the sections from."
+            }
+        },
+        "required": ["page_title"]
+    }
+)
 def wikipedia_page_sections_retriever(page_title: str):
     try:
         page = wikipedia.page(title=page_title, auto_suggest=False)
@@ -20,6 +53,26 @@ def wikipedia_page_sections_retriever(page_title: str):
     except wikipedia.DisambiguationError as e:
         return "Disambiguation required. Call this tool again with one of the following options: " + str(e.options)
 
+
+@register_tool(
+    type = "function",
+    name = "wikipedia_section_content_retriever",
+    description = "Tool that retrieves the content of a specific section of a specific Wikipedia page.",
+    parameters = {
+        "type": "object",
+        "properties": {
+            "page_title": {
+                "type": "string",
+                "description": "The title of the Wikipedia page to retrieve the section content from."
+            },
+            "section_title": {
+                "type": "string",
+                "description": "The title of the section to retrieve the content from."
+            }
+        },
+        "required": ["page_title", "section_title"]
+    }
+)
 def wikipedia_section_content_retriever(page_title: str, section_title: str):
     try:
         page = pages_cache[page_title]
@@ -27,6 +80,26 @@ def wikipedia_section_content_retriever(page_title: str, section_title: str):
     except KeyError:
         return "Page not found in cache. Please use the correct page title as returned by the wikipedia_page_sections_retriever tool."
     
+
+@register_tool(
+    type = "function",
+    name = "wikipedia_similarity_retriever",
+    description = "Tool that retrieves information on Wikipedia based on similarity search with the query.",
+    parameters = {
+        "type": "object",
+        "properties": {
+            "page_title": {
+                "type": "string",
+                "description": "The title of the Wikipedia page to retrieve the information from."
+            },
+            "query": {
+                "type": "string",
+                "description": "The search query for Wikipedia."
+            }
+        },
+        "required": ["page_title", "query"]
+    }
+)
 def wikipedia_similarity_retriever(query: str):
     data = _load_wikipedia_data(query)
     chunks = _split_text_into_chunks(data)
