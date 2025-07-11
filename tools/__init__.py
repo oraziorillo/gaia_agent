c083d13bbf8eba25f1c846bdd6ff02ce5e727daa
oraziorillo <oraziorillo@gmail.com>
2025-07-11 19:23:11 +0200

    simplified logic of importing tools

--- tools/__init__.py
+++ tools/__init__.py
@@ -1,12 +1,8 @@
 # Imports all the tools in the tools directory
-import pkgutil
-__path__ = pkgutil.extend_path(__path__, __name__)
-for importer, modname, ispkg in pkgutil.walk_packages(path=__path__, prefix=__name__ + '.'):
-    if not ispkg: # Only import modules, not subpackages
-        try:
-            __import__(modname)
-        except Exception as e:
-            print(f"Could not import module {modname}: {e}")
+import tools.calculator
+import tools.wikipedia_retrieval
+import tools.web_search
+import tools.youtube_video_analysis
 
 from tools.tool_registry import TOOL_REGISTRY
 __all__ = ['TOOL_REGISTRY'] # Expose only the TOOL_REGISTRY
